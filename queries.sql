bd744023efd6bd7cc6ab11aa74f5af50b28435e9
Anderson Vasconcelos <ciclista-rs@hotmail.com>
2021-05-26 14:59:29 -0300

    Treino SQL

---
+++ queries.sql
@@ -0,0 +1,66 @@
+CREATE TABLE IF NOT EXISTS kopas(
+id BIGSERIAL CONSTRAINT pk_kopas PRIMARY KEY,
+selecao VARCHAR(128) NOT NULL,
+qnt_copas INTEGER
+);
+
+INSERT INTO kopas
+(selecao, qnt_copas)
+VALUES
+('Brasil',5),
+('Alemanha',5),
+('Itália',4),
+('Argentina',2),
+('França',2),
+('Uruguai',2),
+('Inglaterra',1),
+('Espanha',1),
+('Japão',1);
+
+UPDATE kopas SET qnt_copas=4 WHERE selecao='Alemanha';
+UPDATE kopas SET qnt_copas=0 WHERE selecao='Japão';
+
+ALTER TABLE kopas ADD column pts INTEGER AFTER qnt_copas
+
+
+CREATE TABLE IF NOT EXISTS estadios( id BIGSERIAL CONSTRAINT pk_estadios PRIMARY KEY,
+edicao VARCHAR NOT NULL,
+ano VARCHAR(4) NOT NULL,
+estadio VARCHAR NOT NULL,
+LOCAL VARCHAR(128) NOT NULL,
+publico_pagante integer NOT NULL);
+
+
+INSERT INTO estadios
+(edicao,ano,estadio,local,publico_pagante)
+VALUES
+('I','1930','Estádio Centenário','Montevidéu',68346),
+('II','1934','Estádio do Partido Nacional Fascista','Roma',55000),
+('III','1938','Estádio Olímpico Yves-du-Manoir','Colombes',45000),
+('IV','1950','Estádio do Maracanã','Rio de Janeiro',173850),
+('V','1954','Estádio Wankdorf','Berna',62500),
+('VI','1958','Estádio Råsunda','Solna',49737),
+('VII','1962','Estádio Nacional do Chile','Santiago',68679),
+('VIII','1966','Estádio de Wembley','Londres',96924),
+('IX','1970','Estádio Azteca','Cidade do México',107412),
+('X','1974','Estádio Olímpico de Munique','Munique',78200),
+('XI','1978','Estádio Monumental de Núñez','Buenos Aires',71483),
+('XII','1982','Estádio Santiago Bernabéu','Madrid',90000),
+('XIII','1986','Estádio Azteca','Cidade do México',114600),
+('XIV','1990','Estádio Olímpico de Roma','Roma',73603),
+('XV','1994','Rose Bowl','Pasadena',94194),
+('XVI','1998','Stade de France','Saint-Denis',80000),
+('XVII','2002','Estádio Internacional de Yokohama','Yokohama',69029),
+('XVIII','2006','Estádio Olímpico de Berlim','Berlim',69000),
+('XIX','2010','Soccer City','Joanesburgo',84490),
+('XX','2014','Estádio do Maracanã','Rio de Janeiro',74738),
+('XXI','2018','Estádio Lujniki','Moscou',78011);
+
+SELECT * FROM estadios
+
+SELECT * FROM estadios WHERE publico_pagante > 100000
+
+SELECT * FROM estadios WHERE publico_pagante > 50000 AND publico_pagante < 100000;
+
+SELECT * FROM estadios WHERE estadio LIKE '%y'
+
